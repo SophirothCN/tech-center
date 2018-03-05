754d6c6150d8de0da7c6ec2af71d702449ea0bb9
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-05 16:08:30 +0800

    1

--- sophiroth_cluster/cluster_scripts/set_hostname.py
+++ sophiroth_cluster/cluster_scripts/set_hostname.py
@@ -2,19 +2,22 @@
 #coding:utf-8
 import socket,os,re
 #前面是主机名，后面是ip的最后一位地址
-t7={'ip':'77','hostname':'t7.alv.pub'}
-t8={'ip':'78','hostname':'t8.alv.pub'}
-zabbix={'ip':'51','hostname':'zabbix.alv.pub'}
+def makeAlvHost(hostname,ip):
+    return {'ip': ip, 'hostname': hostname+'alv.pub'}
+hostDict={} #Define dict for hosts
+hostDict['zabbix']=makeAlvHost('zabbix','51') #Define host
+hostDict['db1']=makeAlvHost('db1','52')
+hostDict['db2']=makeAlvHost('db2','53')
+hostDict['dc']=makeAlvHost('dc','54')
+hostDict['ansible']=makeAlvHost('ansible','55')
 
-hosts=[t7,t8,zabbix]
 
-ipstr=os.popen('ip a s ens32|grep global').read()
-lastIPNumber=re.findall(r'\w\s(.*)\/',ipstr)[0].split('.')[-1]
-defaultName='os'+str(lastIPNumber)+'.alv.pub' #默认主机名
+ipstr=os.popen('ip a s ens32|grep global').read() #获取关于ip信息的字符串
+lastIPNumber=re.findall(r'\w\s(.*)\/',ipstr)[0].split('.')[-1]  #截取ip最后一位
+defaultName='os'+str(lastIPNumber)+'.alv.pub' #定义默认主机名
 os.system('hostname %s' % defaultName) #设置默认主机名
 os.system('echo %s > /etc/hostname' % defaultName) #设置默认主机名
-for i in hosts:
-    if lastIPNumber == i['ip']:
-        os.system('hostname %s'%i['hostname'])
-        os.system('echo %s > /etc/hostname'%i['hostname'])
+for hostname in hostDict:
+    if hostDict[hostname]['ip'] == lastIPNumber:
+        print(hostDict[hostname]['hostname'])
         break
