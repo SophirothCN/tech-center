e46c14ae6581091bd378e5cd90ce2d3214405574
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-06 11:07:21 +0800

    Alvin updated some content.

---
+++ sophiroth_cluster/dhcp.alv.pub/updateDhcpdFile.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#conding:utf-8
+import os
+url='https://github.com/AlvinWanCN/TechnologyCenter/raw/master/sophiroth_cluster/dhcp.alv.pub/dhcpd.conf'
+file='/etc/dhcp/dhcpd.conf'
+os.system('curl -fsSL %s > %s'%(url,file))
+os.system('systemctl restart dhcpd')
