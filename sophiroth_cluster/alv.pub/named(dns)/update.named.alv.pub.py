edc519801fb86a66a64f2c51d3de9f68243591cb
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-06 11:45:23 +0800

    add update.name.alv.pub.py file, use for update named configuration file.

---
+++ sophiroth_cluster/alv.pub/named(dns)/update.named.alv.pub.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#coding:utf-8
+import os
+url='https://github.com/AlvinWanCN/TechnologyCenter/raw/master/sophiroth_cluster/alv.pub/named(dns)/alv.pub.zone'
+file='/var/named/alv.pub.zone '
+serviceName='named'
+os.system('curl -fsSL %s > %s'%(url,file))
+os.system('systemctl restart %s'%serviceName)
