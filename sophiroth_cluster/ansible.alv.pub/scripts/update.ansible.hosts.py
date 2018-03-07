ed5e7c56763fe040e50c3266bf61512db90706b7
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-07 14:37:55 +0800

    update

--- sophiroth_cluster/ansible.alv.pub/scripts/update.ansible.hosts.py
+++ sophiroth_cluster/ansible.alv.pub/scripts/update.ansible.hosts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #coding:utf-8
 import os
-url='https://github.com/AlvinWanCN/TechnologyCenter/raw/master/sophiroth_cluster/alv.pub/named/alv.pub.zone'
+url='https://raw.githubusercontent.com/AlvinWanCN/TechnologyCenter/master/sophiroth_cluster/ansible.alv.pub/ansible/conf.d/hosts'
 file='/etc/ansible/hosts'
 #serviceName='ansible'
 os.system('curl -fsSL %s > %s'%(url,file))
